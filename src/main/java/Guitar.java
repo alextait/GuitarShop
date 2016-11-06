93bcbe11cba14396e92ab3adfc0ddc2350e80af9
alextait <alextait@gmail.com>
2016-11-06 15:04:19 +0000

    reverted to provided support code

--- src/main/java/Guitar.java
+++ src/main/java/Guitar.java
@@ -1,70 +1,46 @@
-/**
- * Created by alex on 06/11/2016.
- */
 public class Guitar {
-    private String serialNumber;
-    private double price;
-    private String builder;
-    private String model;
-    private String type;
-    private String backWood;
-    private String topWood;
 
-    public String getSerialNumber() {
-        return serialNumber;
-    }
+  private String serialNumber, model;
+  private double price;
+  private Builder builder;
+  private Type type;
+  private Wood backWood, topWood;
 
-    public void setSerialNumber(String serialNumber) {
-        this.serialNumber = serialNumber;
-    }
+  public Guitar(String serialNumber, double price,
+                Builder builder, String model, Type type,
+                Wood backWood, Wood topWood) {
+    this.serialNumber = serialNumber;
+    this.price = price;
+    this.builder = builder;
+    this.model = model;
+    this.type = type;
+    this.backWood = backWood;
+    this.topWood = topWood;
+  }
 
-    public double getPrice() {
-        return price;
-    }
+  public String getSerialNumber() {
+    return serialNumber;
+  }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getBuilder() {
-        return builder;
-    }
-
-    public void setBuilder(String builder) {
-        this.builder = builder;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getBackWood() {
-        return backWood;
-    }
-
-    public void setBackWood(String backWood) {
-        this.backWood = backWood;
-    }
-
-    public String getTopWood() {
-        return topWood;
-    }
-
-    public void setTopWood(String topWood) {
-        this.topWood = topWood;
-    }
-
-
+  public double getPrice() {
+    return price;
+  }
+  public void setPrice(float newPrice) {
+    this.price = newPrice;
+  }
+  public Builder getBuilder() {
+    return builder;
+  }
+  public String getModel() {
+    return model;
+  }
+  public Type getType() {
+    return type;
+  }
+  public Wood getBackWood() {
+    return backWood;
+  }
+  public Wood getTopWood() {
+    return topWood;
+  }
 }
